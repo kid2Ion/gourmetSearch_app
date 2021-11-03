77fb61ec4c848d201670c80b28fb1b50c683d75c
hiroki-kondo-git <hiroki.kondo@zuuonline.com>
2021-11-03 17:07:23 +0900

    create_shop_cache

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.13
 require (
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/mattn/go-sqlite3 v2.0.3+incompatible // indirect
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
 	gorm.io/driver/sqlite v1.2.3
 	gorm.io/gorm v1.22.2
--- main.go
+++ main.go
@@ -21,29 +21,34 @@ type GourmetSearcher struct {
 }
 
 func (g *GourmetSearcher) SearchGourmet(ctx echo.Context) error {
+	// クエリから検索キーワード取得
 	keyWord := ctx.QueryParam("keyword")
 
-	//sqlite検索
+	//sqliteにキャッシュ保持していないか検索
 	shops := db.SearchShopCache(keyWord)
 	if len(shops) != 0 {
+		// cacheにあればjsonをフロントに返す
 		return ctx.JSON(http.StatusOK, shops)
 	} else {
-		// apiから取得
+		// cacheになければapiから取得
 		xml, err := g.callApi(keyWord)
 		if err != nil {
 			return err
 		}
-		shop, err := parse.MyParse(xml)
+		// パース処理
+		shops, err := parse.MyParse(xml)
 		if err != nil {
 			return err
 		}
-		//cacheに書き込む
-		// db.CreateShopCache(keyWord, shop)
-		return ctx.JSON(http.StatusOK, shop)
+		//新たにcacheに書き込む
+		db.CreateShopCache(keyWord, shops)
+		// jsonをフロントに返す
+		return ctx.JSON(http.StatusOK, shops)
 	}
 }
 
 func (g *GourmetSearcher) callApi(keyWord string) ([]byte, error) {
+	// api叩くURI作成
 	url := fmt.Sprintf("https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=%s&keyword=%s&count=20", g.apikey, keyWord)
 
 	resp, err := http.Get(url)
@@ -62,35 +67,21 @@ func (g *GourmetSearcher) callApi(keyWord string) ([]byte, error) {
 }
 
 func main() {
+	// コマンドライン第二引数にapikey
 	if len(os.Args) != 2 {
 		log.Fatalf("usage: %s Apikey", os.Args[0])
 	}
 	apiKey := os.Args[1]
-	// Migrate the schema
+	// db開く
 	shopdb, err := gorm.Open(sqlite.Open("shop.db"), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
 	}
+	// Migrate the schema
 	shopdb.AutoMigrate(&db.ShopCache{})
 	searcher := &GourmetSearcher{apikey: apiKey}
 	e := echo.New()
 	e.Static("/", "vue/dist/")
 	e.GET("/keyword", searcher.SearchGourmet)
 	e.Logger.Fatal(e.Start(":8080"))
-}
-
-func GourmetSerach(APIkey, small_area string) error {
-	url := fmt.Sprintf("https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=%s&small_area=%s", APIkey, small_area)
-
-	resp, err := http.Get(url)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return err
-	}
-	fmt.Println(string(body))
-	return nil
 }
